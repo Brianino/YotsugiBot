fb4585f52fb07f94e690da6a33d9e5f9cbfad2d0
Kyousei <luascriptingtut@gmail.com>
2017-09-13 23:54:43 +0200

    New -- Console Logging
    The bot logs if the command was ran successfully or if it failed in teh console.

--- YotsugiMain.py
+++ YotsugiMain.py
@@ -6,14 +6,7 @@ embed_color = 0xFFFFF
 #Embed color is customizable. Put it like this: 0xYOUR-HEX-CODE
 
 #Do NOT edit past this point!!!#
-#---------------------------------------#
-#---------------------------------------#
-#---------------------------------------#
-#---------------------------------------#
-#---------------------------------------#
-#---------------------------------------#
-#---------------------------------------#
-#---------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------#
 import discord
 import asyncio
 import random
@@ -32,22 +25,29 @@ start_time = time.time()
  
 @client.event
 async def on_ready():
-    print("Bot Logged In, Running")
-    print("Name: {}".format(client.user.name))
-    print("ID: {}".format(client.user.id))
-    print("-------------------------------------")
+    print("Logging In...")
+    time.sleep(4)
+    print("Checking files..")
+    time.sleep(2)
+    print("Logged In | Client Credentials")
+    print(Fore.YELLOW + "\n       Client Name: {}".format(client.user.name) +"\n       Client ID: {}".format(client.user.id) + "\n       Prefix: {}".format(prefix) + "\n       Embed Color: {}".format(embed_color) + "\n       Version: {}".format(ver) + "\n       Owner ID: {}".format(owner))
     await client.change_presence(game=discord.Game(name=''))
 
 
 @client.command(pass_context = True)
 async def send(ctx, member : discord.Member, *, message):
-        if ctx.message.author.id != owner: return await client.say(":x: You are not the bot owner")
-        if ctx.message.author.id == owner: return await client.send_message(member, message)
+        if ctx.message.author.id != owner:
+            return await client.say(":x: You are not the bot owner")
+            print(Fore.RED + "Command Failed To Execute |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason:  " + Fore.YELLOW + "Not bot owner!")
+        if ctx.message.author.id == owner:
+            return await client.send_message(member, message)
+            print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 @client.command(pass_context = True)
 async def h(ctx):
     embed = discord.Embed(description = "**Hosting Guides: https://github.com/Kyousei/YotsugiBot/wiki** \n**Commands List: https://goo.gl/w6Aoag**", color = embed_color)
-    await client.say(embed = embed) 
+    await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 @client.command
 async def h(command = None):
@@ -59,18 +59,20 @@ async def h(command = None):
     return
 
  
-@client.command()
-async def ping():
+@client.command(pass_context = True)
+async def ping(ctx):
     pingtime = time.time()
     pingms = await client.say("Pinging...")
     ping = time.time() - pingtime
     await client.edit_message(pingms, "Pong! :ping_pong:  The ping time is `%.01f seconds`" % ping)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
  
 #command1
 @client.command(pass_context = True)
 async def invite(ctx):
     embed = discord.Embed(title = "Here are invite links:", description = "Invite me to your server with this link: https://discordapp.com/oauth2/authorize?client_id=331766751765331969&scope=bot&permissions=66186303", color = embed_color)
     return await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
  
 #command2
 @client.command(pass_context = True, no_pm = True)
@@ -79,6 +81,7 @@ async def banlist(ctx):
     x = '\n'.join([y.name for y in x])
     embed = discord.Embed(title = "List of Banned Members", description = x, color = embed_color)
     return await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
  
 #command3
 @client.command(pass_context=True, no_pm = True)
@@ -88,6 +91,7 @@ async def connect(ctx):
     author = ctx.message.author
     voice_channel = author.voice_channel
     vc = await client.join_voice_channel(voice_channel)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
  
 #command4
 @client.command(pass_context = True, no_pm = True)
@@ -95,6 +99,7 @@ async def disconnect(ctx):
     for x in client.voice_clients:
         if(x.server == ctx.message.server):
             return await x.disconnect()
+            print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
         
 #command6
 @client.command(pass_context=True, no_pm = True)       
@@ -102,11 +107,13 @@ async def clear(ctx, number):
     embed = discord.Embed(description = ":x: Insufficient permissions! You require **Manage Messages** permission in order to clear messages!", color = 0xF00000)
     if not ctx.message.author.server_permissions.manage_messages:
         return await client.say(embed = embed)
+        print(Fore.CYAN + "Command Failed To Execute |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason: " + Fore.YELLOW  + "Insufficient Permissions! Both user and bot need Manage Messages permission!")
     mgs = []
     number = int(number) #Converting the amount of messages to delete to an integer
     async for x in client.logs_from(ctx.message.channel, limit = number):
         mgs.append(x)
     await client.delete_messages(mgs)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 bot_version = 'v0.4.4'
 #command8
@@ -115,6 +122,7 @@ bot_author = 'Kyousei#8357'
 async def author(ctx):
     embed = discord.Embed(title = "Yotsugi Bot Author:", description = "Name: **" + bot_author + "**  \nJoined Discord: **07.02.2016  1:10 PM**  \n  **ID**: 145878866429345792  \n**Email**: yotsugibot@gmail.com  \nSay **;h** for commands.", color = embed_color)
     return await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 #command9
 @client.command(pass_context = True, no_pm = True)
@@ -131,9 +139,11 @@ async def ban(ctx, *, member : discord.Member = None):
         if 'Privilege is too low' in str(e):
             embed = discord.Embed(description = "Privilege is too low. :x:", color = 0xF00000)
             return await client.say(embed = embed)
+            print(Fore.RED + "Command Failed To Execute |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason: " + Fore.YELLOW + "Insufficient Permissions! Both user and bot need Ban Members permission!")
 
     embed = discord.Embed(description = "**%s** has been banned."%member.name, color = 0xF00000)
     return await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 
 #command10
@@ -150,29 +160,33 @@ async def kick(ctx, *, member : discord.Member = None):
         if 'Privilege is too low' in str(e):
             embed = discord.Embed(description = "Privilege is too low. :x:", color = 0xF00000)
             return await client.say(embed = embed)
+            print(Fore.RED + "Command Failed To Execute |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason: " + Fore.YELLOW + "Inusfficient Permissions! Both user and bot need Kick Members permission!")
 
     embed = discord.Embed(description = "**%s** has been kicked."%member.name, color = 0xF00000)
     return await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 	
 #command12
 @client.command(pass_context = True, no_pm = True)
 async def mute(ctx, *, member : discord.Member):
     if not ctx.message.author.server_permissions.mute_members:
-        return
+        return print(Fore.RED + "Command Failed To Execute |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason: " + Fore.YELLOW + "Insufficient Permissions! Both user and member need Mute Members permission!")
     overwrite = discord.PermissionOverwrite()
     overwrite.send_messages = False
     await client.edit_channel_permissions(ctx.message.channel, member, overwrite)
     await client.say("**%s** has been muted!"%member.mention)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 #command13
 @client.command(pass_context = True, description='Unmutes the muted members.', no_pm = True)
 async def unmute(ctx, *, member : discord.Member):
     if not ctx.message.author.server_permissions.mute_members:
-        return
+        return print(Fore.RED + "Command Failed To Execute |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason: " + Fore.YELLOW + "Insufficient Permissions! Both user and bot need Mute Members permission!")
     overwrite = discord.PermissionOverwrite()
     overwrite.send_messages = True
     await client.edit_channel_permissions(ctx.message.channel, member, overwrite)
     await client.say("**%s** has been unmuted!"%member.mention)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 #command14
 answers = ["My source say no.", "I completely disagree.", "No way in hell!", "Sure! :D", "Why not?", "Why would you say that?", "When life gives you lemons, throw them at people!", "HA, You wish!", "Keep dreaming!", "Does a green light mean go?", "Red is supposed to stop you, but your magic is TOO strong! :sweat:", "Power outage!??! WHAT ABOUT MY WIFI!??!!", "Hmmm.. this is hard", "lol, just lol.", "Cleverbot is no match for me! Haahahaha", "The chances of that happening are equal to the chances of shivaco getting a girlfriend. Null!", "There's an admin watching :scream:", "Ask me tomorrow :zzz:", "No... I mean yes... Well... Ask again later"]
@@ -206,11 +220,12 @@ async def roll(dice : str):
 
 
 #command17
-@client.command()
-async def github():
+@client.command(pass_context = True)
+async def github(ctx):
     """  ---Link to Github"""
     embed = discord.Embed(description = "Yotsugi Github can be found here: https://github.com/YotsugiBot", color = embed_color)
     await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 
 #command18
@@ -220,6 +235,7 @@ async def servers(ctx):
     print(x)
     embed = discord.Embed(title = "Servers", description = x, color = embed_color)
     return await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 
 @client.command(no_pm = False)
@@ -231,6 +247,7 @@ async def stats():
     week, day = divmod(day, 7)
     embed = discord.Embed(title = "Yotsugi **" + bot_version + "**", description = "**Author: **" + bot_author + " \n\n\n **Uptime:** \n**%d** weeks, \n**%d** days, \n**%d** hours, \n**%d** minutes, \n**%d** seconds"% (week, day, hour, minute, second), color = embed_color)
     await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 
 @client.command(pass_context=True, no_pm=True)
@@ -238,6 +255,7 @@ async def shutdown(ctx):
     if owner == ctx.message.author.id:
         embed = discord.Embed(description = "Shutting Down...", color = embed_color)
         await client.say(embed = embed)
+        print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
         await client.logout()
         
 
@@ -245,11 +263,13 @@ async def shutdown(ctx):
 async def serverid(ctx, *, member = discord.Member):
     embed = discord.Embed(description = ctx.message.author.mention + ", ID of this server is:** " + ctx.message.channel.server.id + "**", color = embed_color)
     await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 @client.command(pass_context = True, no_pm = True)
 async def channelid(ctx):
     embed = discord.Embed(description = ctx.message.author.mention + ", ID of this channel is:** " + ctx.message.channel.id + "**", color = embed_color)
     await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 
 @client.command(pass_context=True, no_pm=True)
@@ -258,9 +278,11 @@ async def removerole(ctx, user: discord.Member, *, role):
         await client.remove_roles(user, discord.utils.get(ctx.message.server.roles, name=role))
         embed = discord.Embed(description = ("Removed %s from **%s**" % (user.mention, role)), color = embed_color)
         await client.say(embed = embed)
+        print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
     else:
         embed = discord.Embed(description = ":x: Insufficient permissions!", color = 0xFF0000)
         return await client.say(embed = embed)
+        print(Fore.RED + "Command Failed To Execute |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason: " + Fore.YELLOW + "Insufficient Permissions! Both user and bot need Manage Roles permission!")
 
 
 @client.command(pass_context=True, no_pm=True)
@@ -269,17 +291,21 @@ async def setrole(ctx, user: discord.Member, *, role):
         await client.add_roles(user, discord.utils.get(ctx.message.server.roles, name=role))
         embed = discord.Embed(description = ("Added %s to  **%s** " % (user.mention, role)), color = embed_color)
         await client.say(embed = embed)
+        print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
     else:
         embed = discord.Embed(description = ":x: Insufficient permissions!", color = 0xFF0000)
         return await client.say(embed = embed)
+        print(Fore.RED + "Command Failed To Execute |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason: " + Fore.YELLOW + "Insufficient Permissions! Both user and bot need Manage Roles permission!")
 
 @client.command(pass_context = True, no_pm = True)
 async def warn(ctx, member : discord.Member, *, message):
         if ctx.message.author.id != owner:
                 return print("User **" + ctx.message.author.id + "** tried to use :warn in **" + ctx.message.channel.server.id + "**! It did NOT work because they are not the owner")
+                print(Fore.RED + "Command Failed To Execute |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason: " + Fore.YELLOW + "Not owner!")
         if ctx.message.author.id == owner:
                 embed = discord.Embed(description = "You've been warned for: **" + message + "**\nResponsible Moderator: **" + ctx.message.author.mention + "**\nServer: **" + ctx.message.server.name + "**", color = 0xFF0000)
                 return await client.send_message(member, embed = embed)
+                print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 
 @client.command()
@@ -296,6 +322,7 @@ async def ud(*msg):
     embed.set_footer(text = "Tags: " + ', '.join(response['tags']))
 
     await client.say(embed = embed)
+    print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
 
 
 
@@ -307,8 +334,10 @@ async def setgame(ctx, *, game : str):
             try:
                 await client.change_presence(game=discord.Game(name=game), status=ctx.message.server.me.status)
                 logging.info("Set game to " + str(game))
+                print(Fore.CYAN + "Command Successfully Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]")
             except Exception as e:
                 print("Failed to set game: {}".format(str(e)) + "\nIgnore this error. It's Python who's being an ass.")
+                print(Fore.RED + "Command Error Raised, But The Command Was Still Executed |\n       Command Ran In:[" + ctx.message.server.id + "]\n       User:[" + ctx.message.author.id + "]\n       Channel:[" + ctx.message.channel.id + "]\n       Reason: " + Fore.YELLOW + "Ignore this error!")
     
 
 
